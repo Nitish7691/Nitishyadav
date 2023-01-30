232033476684d7540ef2f2be387ffc627a179d53
Nitish7691 <111192893+Nitish7691@users.noreply.github.com>
2023-01-31 02:50:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,19 +7,19 @@ load_dotenv()
 
 
 # Mandatory variables for the bot to start
-API_ID = int(os.getenv("API_ID", "Your Api Id"))
-API_HASH = os.environ.get("API_HASH", "Your Api Hash")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "Bot Token")
-ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("Owner Id")] if os.environ.get("ADMINS") else []
+API_ID = int(os.getenv("API_ID", "23388932"))
+API_HASH = os.environ.get("API_HASH", "fb0ca41bae7937090bc6aba2f6f85a29")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5652432118:AAEAjrQVwp-o-ZHhMcGtkuSpdmhoXkZX2Dc")
+ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("5309651681")] if os.environ.get("ADMINS") else []
 ADMIN = ADMINS
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "Db Name")
-DATABASE_URL = os.getenv("DATABASE_URL", "Monfo url") 
-OWNER_ID =  int(os.environ.get("OWNER_ID", "Owner Id")) 
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "earnspace7691")
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://earnspace7691:8hZTEqKu5hGh!R#@cluster0.8k4dii4.mongodb.net/?retryWrites=true&w=majority") 
+OWNER_ID =  int(os.environ.get("OWNER_ID", "5309651681")) 
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
-ADMINS.append(Id Owned Id)
+ADMINS.append(5309651681)
 #  Optionnal variables
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "Logs Channels Id")) 
-UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "Updates Channel User name Without @") # For Force Subscription
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "1695261980")) 
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "earnspaceofficial") # For Force Subscription
 BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "True") # true if forward should be avoided
 WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '') # image when someone hit /start # image when someone hit /start
 LINK_BYPASS = "False"
